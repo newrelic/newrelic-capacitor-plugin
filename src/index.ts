0edfc15bfdb058afb93f2f23e46804140f7c9b94
Kenny <kthai@newrelic.com>
2022-11-15 12:48:11 -0800

    chore: add license headers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 
+ */
+
 import { registerPlugin } from '@capacitor/core';
 
 import type { NewRelicCapacitorPluginPlugin } from './definitions';
