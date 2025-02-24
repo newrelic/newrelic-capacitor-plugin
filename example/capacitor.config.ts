786cb5e1fa826b3691dace80959e8e58df9e8913
Marty Wang <mwang@incomm.com>
2025-02-24 14:10:44 -0700

    update to capacitor 7

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'newrelic-example-capacitor-app',
   webDir: 'build',
-  bundledWebRuntime: false
 };
 
 export default config;
