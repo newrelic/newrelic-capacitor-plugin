c954b72cd820fa87e2f6825f4af747c41e173f7b
ndesai-newrelic <89222514+ndesai-newrelic@users.noreply.github.com>
2026-01-12 14:30:29 -0600

    fix the package swift depedency
    plugin is unable to download the package depedencies

--- Package.swift
+++ Package.swift
@@ -3,40 +3,22 @@ import PackageDescription
 
 let package = Package(
     name: "NewrelicNewrelicCapacitorPlugin",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "NewrelicNewrelicCapacitorPlugin",
             targets: ["NewrelicNewrelicCapacitorPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/newrelic/newrelic-ios-agent-spm.git", from: "7.6.0")
     ],
     targets: [
         .target(
-            name: "NewrelicNewrelicCapacitorPluginObjC",
-            dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorCordova", package: "capacitor-swift-pm")
-            ],
-            path: "ios/Plugin",
-            exclude: [
-                "Info.plist",
-                "NewRelicCapacitorPlugin.swift",
-                "NewRelicCapacitorPluginPlugin.swift"
-            ],
-            sources: [
-                "NewRelicCapacitorPluginPlugin.m"
-            ],
-            publicHeadersPath: "Public"
-        ),
-        .target(
             name: "NewrelicNewrelicCapacitorPlugin",
             dependencies: [
-                "NewrelicNewrelicCapacitorPluginObjC",
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "CapacitorCordova", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "NewRelic", package: "newrelic-ios-agent-spm")
             ],
             path: "ios/Plugin",
