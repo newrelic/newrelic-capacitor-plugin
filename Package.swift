fec93ef91c36479f52363abbd925e3e5e7a4a2e9
ha6-6ru <20640973+ha6-6ru@users.noreply.github.com>
2026-01-14 14:17:36 +0900

    migrate to capacitor 8

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "NewrelicNewrelicCapacitorPlugin",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "NewrelicNewrelicCapacitorPlugin",
             targets: ["NewrelicNewrelicCapacitorPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://github.com/newrelic/newrelic-ios-agent-spm.git", from: "7.6.0")
     ],
     targets: [
